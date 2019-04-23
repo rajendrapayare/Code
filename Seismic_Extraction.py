9aa14d5e2c15d820138186b82789c92b7c3b534f
RAJENDRA <rajendrapayare@yahoo.com>
2019-04-23 14:48:33 +0400

    mytest3

--- Seismic_Extraction.py
+++ Seismic_Extraction.py
@@ -71,6 +71,9 @@ G_NEW_SOMEFEATEUERW- "Rajendra"
 # My some feature code @ testing branch next code
 G_NEW_SOMEFEATEUERW- "Rajendra"
 
+# My some feature code @ testing branch next code only in master
+G_NEW_SOMEFEATEUERW- "Rajendra"
+
 DICT_GEOM_FROM_CSV = False
 DICT_PPDM_FROM_CSV = False
 DICT_MINMAX_FROM_CSV = False
